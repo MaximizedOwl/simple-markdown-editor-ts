c2d725188efe9dcaeb3921cf1879d0dff3ee6097
MaximizedOwl <mkarasawaaa+0@gmail.com>
2023-03-02 15:19:51 +0900

    npm test初回成功時

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('サイトタイトル', () => {
+  test('名前が「Simple Markdown Editor」になっているか', () => {
+    render(<App />);
+  });
 });
