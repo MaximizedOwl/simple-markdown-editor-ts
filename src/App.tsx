f81bab1811bcebb5b69c60f7d13b2cdeb54c2c81
MaximizedOwl <mkarasawaaa+0@gmail.com>
2023-03-02 17:40:15 +0900

    muiとjoy uiのインストールしデザインの変更、ボタンの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,56 @@
+import Textarea from '@mui/joy/Textarea';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 import { useState } from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import './App.css';
 
 function App() {
-  const [str, setStr] = useState('');
+  const [str, setStr] = useState(`# Hello world
+  - foo
+  - buzz`);
 
   return (
     <div className='App'>
-      <header className='App-header'>
-        <div>
-          <h1>Simple Markdown Editor</h1>
+      <header>
+        <div className='header'>
+          <div className='header-title'>
+            <h2>Simple Markdown Editor</h2>
+          </div>
+          <div className='header-menu'>
+            <Stack direction='row' spacing={2}>
+              <Button variant='outlined'>share</Button>
+              <Button variant='outlined'>❔</Button>
+              <Button variant='outlined'>🌛</Button>
+            </Stack>
+          </div>
         </div>
       </header>
       <main>
-        <div className='main-content'>
-          <div className='input'>
-            <textarea
+        <div className='main'>
+          <div className='main-input'>
+            <Textarea
+              placeholder='# type something'
+              maxRows={20}
+              size='lg'
+              variant='outlined'
               name='postContent'
-              id='textarea-input'
+              className='textarea-input'
               value={str}
               onChange={(e) => setStr(e.target.value)}
+              sx={{
+                minWidth: '40vw',
+                minHeight: '70vh',
+                maxWidth: '40vw',
+                maxHeight: '70vh',
+              }}
             />
           </div>
-          <div className='output'>
+          <div className='main-output'>
             <label id='label-output'>
               <ReactMarkdown
-                className='reactmarkdown'
+                className='main-reactmarkdown'
                 remarkPlugins={[remarkGfm]}>
                 {str}
               </ReactMarkdown>
@@ -34,6 +58,15 @@ function App() {
           </div>
         </div>
       </main>
+      <footer>
+        <div className='footer'>
+          <div className='footer-menu'>
+            <Stack direction='row' spacing={2}>
+              <Button variant='outlined'>Gist</Button>
+            </Stack>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
